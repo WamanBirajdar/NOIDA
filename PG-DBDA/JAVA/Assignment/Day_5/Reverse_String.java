c6b74f9aa4adfe77c4778df930da3f9f39ba7352
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2022-11-09 23:19:34 +0530

    Update Reverse_String.java

--- PG-DBDA/JAVA/Assignment/Day_5/Reverse_String.java
+++ PG-DBDA/JAVA/Assignment/Day_5/Reverse_String.java
@@ -1,1 +1,16 @@
-
+     String str, strReverse="";
+      int len, i;
+      char ch;
+      Scanner s = new Scanner(System.in);
+      
+      System.out.print("Enter the String: ");
+      str = s.nextLine();
+      
+      len = str.length();
+      for(i=(len-1); i>=0; i--)
+      {
+         ch = str.charAt(i);
+         strReverse = strReverse + ch;
+      }
+      
+      System.out.println("\nReverse = " +strReverse);
