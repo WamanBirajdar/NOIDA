619b3ec7d722a774b8dc332a34d86d3b103c5806
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2022-11-19 16:20:13 +0530

    Create ArrayExample2.java

---
+++ PG-DBDA/JAVA/GuessOutput/ArrayExample2.java
@@ -0,0 +1,14 @@
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		
+		int a[]= {0,1,2,3,4,5,6,7,8,9};
+		
+		int n=6;
+		n=a[a[n]/2];
+		System.out.println(n);
+  }
+}
+
+//ouput
+// 3
