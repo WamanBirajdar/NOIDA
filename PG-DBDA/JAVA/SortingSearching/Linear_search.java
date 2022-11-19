b9a88105c004fbaad9fe6984ffcbd9066886a468
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2022-11-19 11:22:57 +0530

    Create Linear_search.java

---
+++ PG-DBDA/JAVA/SortingSearching/Linear_search.java
@@ -0,0 +1,57 @@
+/*
+llinear search in array 
+*/
+package Day_1;
+
+import java.util.*;
+
+public class Linear_Example {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the array elemts");
+		int n=s.nextInt();
+		int i;
+		//initialization of array 
+		int a[]=new int[n];
+		
+		
+		//inserting elements in array
+		for(i=0;i<a.length;i++)
+		{
+			System.out.println("Insert element in array");
+			a[i]=s.nextInt();
+		}
+		
+		//Display the array elements 
+		for(i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+		
+		//enter the number to be search in array
+		System.out.println("Enter the number to be search");
+		int n1=s.nextInt();
+		
+		//searching number in array using linear search
+		for(i=0;i<a.length;i++)
+		{
+			if(a[i]==n1)
+			{
+				System.out.println("Key found" );
+				break;
+			}
+						
+		}
+		if(i==n)
+		{
+			System.out.println("Key not found");
+		}
+		
+		
+
+
+	}
+
+}
