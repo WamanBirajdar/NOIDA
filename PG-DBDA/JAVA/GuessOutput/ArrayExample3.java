229a6ba7da671a20727d64b1a4cc13d98874ddac
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2022-11-19 16:24:10 +0530

    Create ArrayExample3.java

---
+++ PG-DBDA/JAVA/GuessOutput/ArrayExample3.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		
+		int arr[];
+		arr=new int[2];
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+	}
+
+}
+
+//ouput
+/* 0
+   0
+*/
