e65e371a1c7d889fc6a8354d04f17612c1f30826
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2022-11-28 17:05:32 +0530

    Update AlphabetPattern.py

--- PG-DBDA/Python/AlphabetPattern.py
+++ PG-DBDA/Python/AlphabetPattern.py
@@ -1,3 +1,12 @@
+#print the following pattern
+   """
+A 
+B B 
+C C C
+D D D D
+E E E E E"""
+    
+    
 ch=65
 for i in range(5):
     for j in range(5):
@@ -6,10 +15,22 @@ for i in range(5):
     ch+=1
     print()
     
+'''
+1 
+2 2 
+3 3 3
+4 4 4 4
+5 5 5 5 5
+'''
     
-   """
-A 
-B B 
-C C C
-D D D D
-E E E E E"""
+r=5
+c=5
+for i in range(1,r+1):
+    for j in range(1,c+1):
+        if i>=j:
+            print(i,end=" ")
+    print()
+    
+
+    
+
