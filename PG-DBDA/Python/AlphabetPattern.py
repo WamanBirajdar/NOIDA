0e80ee7d8b045ee62f8dbb7ff27ff3bc9fdd3e76
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2022-11-28 16:20:21 +0530

    Create AlphabetPattern.py

---
+++ PG-DBDA/Python/AlphabetPattern.py
@@ -0,0 +1,15 @@
+ch=65
+for i in range(5):
+    for j in range(5):
+        if i>=j:
+            print(chr(ch),end=" ")
+    ch+=1
+    print()
+    
+    
+   """
+A 
+B B 
+C C C
+D D D D
+E E E E E"""
