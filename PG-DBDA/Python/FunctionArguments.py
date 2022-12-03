c9d9cc3b4d8b5a33ff14d067b6f394fd436a2d93
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2022-12-03 23:01:23 +0530

    Update FunctionArguments.py

--- PG-DBDA/Python/FunctionArguments.py
+++ PG-DBDA/Python/FunctionArguments.py
@@ -9,3 +9,22 @@ add(2,3)
 add(2,3,4)
 add(2,3,4,5)
 add(2,3,4,5,6)
+
+
+#arbitrary keyword arguments
+
+def add(**kwargs):
+    print(kwargs,type(kwargs))
+
+add(a=10,b=20,c=30)
+
+
+def add(**kwargs):
+    sum=0
+    for number in kwargs.values():
+        sum+=number
+    print(f'Total summation of nnumber is:{sum}')
+
+add(a=10,b=20)
+add(a=10,b=20,c=30)
+add(a=10,b=20,c=30,d=40)
