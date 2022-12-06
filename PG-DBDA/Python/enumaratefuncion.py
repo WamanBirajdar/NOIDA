cfaddf8767f25e73ce15f7893fee8d234c9e79b6
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2022-12-06 23:39:41 +0530

    Update enumaratefuncion.py

--- PG-DBDA/Python/enumaratefuncion.py
+++ PG-DBDA/Python/enumaratefuncion.py
@@ -2,3 +2,15 @@
 list1=[10,20,30,40,50]
 for i in enumerate(list1):
     print(i)
+    
+#enumerate function
+list1=[1,2,3,4,5,6,7,8]
+for i in enumerate(list1):
+    print(i)
+
+#ennumerate function
+
+for index , item in enumerate(list1):
+    if item %2==0:
+        list1[index]='even'
+print(list1)
